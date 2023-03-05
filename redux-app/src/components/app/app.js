8ad27b2a47e954685d42f8f6df094441b81c52d8
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-03-05 17:19:58 +0500

    news

---
+++ redux-app/src/components/app/app.js
@@ -0,0 +1,15 @@
+import { Counter } from "../"
+
+
+const App = () => {
+
+
+  return (
+    <div className="container">
+      <h1>Counter: 0</h1>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ redux-app/src/components/counter/counter.js
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <div className="btn-group">
+        <button className="btn btn-primary">Plus</button>
+        <button className="btn btn-secondary">Minus</button>
+        <button className="btn btn-danger">Reset</button>
+    </div>
+  )
+}
+
+export default Counter
